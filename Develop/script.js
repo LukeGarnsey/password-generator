df3470f7feacd1986ef856ecba1858184ad843ea
Luke Garnsey <lukegarnsey@gmail.com>
2023-12-21 10:40:00 -0500

    Added prompt for user to select length of password.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 var letters = "abcdefghijklmnopqrstuvwxyz";
-var size = 8;
+// var size = 8;
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
@@ -12,12 +12,21 @@ function writePassword() {
 
 }
 function generatePassword(){
+  let size = GetPasswordSize();
   password = "";
   while(password.length < size){
     password += letters[Math.floor(Math.random() * letters.length)];
   }
   return password;
 }
-
+function GetPasswordSize(){
+  let size = parseInt(prompt("How long should the password be? (8 - 128)"), 0);
+  // console.log(size);
+  if(Number.isNaN(size) || size < 8 || size > 128){
+    size = GetPasswordSize();
+  }
+  
+  return size;
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
